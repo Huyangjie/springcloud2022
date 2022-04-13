fa0bcf5814469c2fe2034d21bcb435d8e4510cd6
Huyj <Leon_945@163.com>
2022-04-14 00:36:25 +0800

    dev test

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/cn/leon/service/Pay.java
@@ -0,0 +1,14 @@
+package cn.leon.service;
+
+/**
+ * @author Leon
+ * @create 2022-04-14 0:35
+ * IntelliJ IDEA
+ * cn.leon.service
+ */
+public class Pay {
+    public static void main(String[] args) {
+        System.out.println("master1");
+        System.out.println("master2");
+    }
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/cn/leon/service/PaymentService.java
@@ -0,0 +1,13 @@
+package cn.leon.service;
+
+/**
+ * @author Leon
+ * @create 2022-04-14 0:34
+ * IntelliJ IDEA
+ * cn.leon.service
+ */
+public class PaymentService {
+    public static void main(String[] args) {
+        System.out.println("hello master");
+    }
+}
