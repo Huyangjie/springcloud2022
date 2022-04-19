8a4f283b9a8fad83f5b8838916c97ff4d60d62c2
HYJ <Leon_945@163.com>
2022-04-19 19:24:02 +0800

    seata-acctor2003seata启动成功

--- seata-account-service2003/src/main/java/cn/leon/config/DataSourceProxyConfig.java
+++ seata-account-service2003/src/main/java/cn/leon/config/DataSourceProxyConfig.java
@@ -2,20 +2,18 @@ package cn.leon.config;
 
 /**
  * @author Leon
- * @create 2022-04-18 18:31
+ * @create 2022-04-19 17:55
  * IntelliJ IDEA
  * cn.leon.config
  */
+
 import com.alibaba.druid.pool.DruidDataSource;
 import io.seata.rm.datasource.DataSourceProxy;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
@@ -27,27 +25,27 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceProxyConfig {
 
-    @Value("${mybatis.mapperLocations}")
-    private String mapperLocations;
+    private final ApplicationContext applicationContext;
+
+
+    public DataSourceProxyConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
 
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource druidDataSource(){
-        return new DruidDataSource();
+    public DruidDataSource ds0() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        return druidDataSource;
     }
 
-    @Bean
-    public DataSourceProxy dataSourceProxy(DataSource dataSource) {
-        return new DataSourceProxy(dataSource);
-    }
 
+    @Primary
     @Bean
-    public SqlSessionFactory sqlSessionFactoryBean(DataSourceProxy dataSourceProxy) throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSourceProxy);
-        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocations));
-        sqlSessionFactoryBean.setTransactionFactory(new SpringManagedTransactionFactory());
-        return sqlSessionFactoryBean.getObject();
+    public DataSource dataSource(DruidDataSource ds0)  {
+        DataSourceProxy pds0 = new DataSourceProxy(ds0);
+        return pds0;
     }
 
 }
